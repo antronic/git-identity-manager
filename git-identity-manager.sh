6c971d2b1f42b6e5f0e90ce259ce2afbdcd5b8d4
Job Jirachai (MDB) <jirachai.c@mongodb.com>
2026-04-23 14:47:15 +0700

    chore: bump version to v1.2.0

--- git-identity-manager.sh
+++ git-identity-manager.sh
@@ -2,12 +2,12 @@
 
 # ==============================================================================
 #  GIT IDENTITY MANAGER
-#  Version: 1.1.0
+#  Version: 1.2.0
 #  Repository: https://github.com/antronic/git-identity-manager
 #  Features: Setup, Import, Switch, View, Modify, Delete, Keys, Doctor, CLI
 # ==============================================================================
 
-VERSION="1.1.0"
+VERSION="1.2.0"
 UPDATE_URL="https://raw.githubusercontent.com/antronic/git-identity-manager/main/git-identity-manager.sh"
 CHANGELOG_URL="https://api.github.com/repos/antronic/git-identity-manager/releases/latest"
 
